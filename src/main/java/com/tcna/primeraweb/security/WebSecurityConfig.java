c8850923c7914bddcb51a71b21a7161e8a57be44
Your Name <you@example.com>
2025-06-17 16:35:12 -0500

    mejorandolo mas

--- src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
+++ src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig {
                         //rutas de administración (CRUD Categorías, Preguntas y Usuarios)
                         .requestMatchers("/categorias", "/categorias/**").hasAuthority("ADMIN")
                         .requestMatchers("/preguntas", "/preguntas/**").hasAuthority("ADMIN")
-                        .requestMatchers("/usuarios", "usuarios/**").hasAuthority("ADMIN")
+                        .requestMatchers("/usuarios", "/usuarios/**").hasAuthority("ADMIN")
 
                         //rutas de juego (trivia)
                         .requestMatchers("/jugar", "/jugar/**").hasAnyAuthority("USER", "ADMIN")
